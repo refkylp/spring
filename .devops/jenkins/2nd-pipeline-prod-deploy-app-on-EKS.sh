409fae6db09ba492f67bfadb2c9d3829fbbe43e5
refkylp <kayaalp.refia@gmail.com>
2025-08-14 12:17:03 +0000

    deploy.sh updated

--- .devops/jenkins/2nd-pipeline-prod-deploy-app-on-EKS.sh
+++ .devops/jenkins/2nd-pipeline-prod-deploy-app-on-EKS.sh
@@ -12,7 +12,7 @@ helm package .devops/k8s/petclinic_chart
 AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic
 
 kubectl create ns petclinic-prod || echo "namespace petclinic-prod already exists"
-kubectl delete secret regcred -n petclinic-prod|| echo "there is no regcred secret in petclinic-prod namespace"
+kubectl delete secret regcred -n petclinic-prod || echo "there is no regcred secret in petclinic-prod namespace"
 
 kubectl create secret generic regcred -n petclinic-prod \
     --from-file=.dockerconfigjson=/var/lib/jenkins/.docker/config.json \
